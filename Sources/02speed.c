e3bfdd9fda8c19ccc46ee625987abc8fdf0a4d40
WuShun <54wushun@tongji.edu.cn>
2016-07-17 21:46:59 +0800

    提高速度，调整参数

--- Sources/02speed.c
+++ Sources/02speed.c
@@ -13,9 +13,9 @@ int csl_cnt[3]={0,0,0},csr_cnt[3]={0,0,0};
 int targetspeed=0,Motor_PWM_MAX=450,Motor_PWM_MIN=-400;
 float csxs=0.6;//差速系数
 //**********************变速参数***************************/
-int straightspeed=220,transspeed=190,turnspeed=180,deadspeed=180,barspeed=180;//250,190,180
+int straightspeed=230,transspeed=200,turnspeed=190,deadspeed=190,barspeed=200;//250,190,180
 //**********************差速参数***************************/
-signed int Speed_kc1=13000,Speed_kc2=1300;//170-17000  180 15000,
+signed int Speed_kc1=10000,Speed_kc2=1300;//170-17000  180 15000,
 signed int wheel_distance=9;//半车距8
 signed int RPID=0;
 float r=0;
